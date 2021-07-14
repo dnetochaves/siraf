925b3c928536f24709014d56093c09022595d107
dnetochaves <dnetochaves@gmail.com>
2021-07-14 09:29:47 -0300

    Model Perfil

--- apps/perfil/models.py
+++ apps/perfil/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Perfil(models.Model):
+    name = models.CharField(max_length=100)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
