926ddf32682cae63a662bc2b1fc19c6f347553df
dnetochaves <dnetochaves@gmail.com>
2021-07-17 08:54:57 -0300

    Crud Juridico

--- apps/juridico/urls.py
+++ apps/juridico/urls.py
@@ -6,5 +6,8 @@ app_name = "juridico"
 urlpatterns = [
     path('', views.juridico, name="juridico"),
     path('item_contratos/<int:id>/', views.item_contratos, name="item_contratos"),
+    path('listar_contratos/', views.listar_contratos, name="listar_contratos"),
     path('novo_contrato/', views.novo_contrato, name="novo_contrato"),
+    path('editar_contrato/<int:id>/', views.editar_contrato, name="editar_contrato"),
+    path('deletar_contrato/<int:id>/', views.deletar_contrato, name="deletar_contrato"),
 ]
--- apps/juridico/views.py
+++ apps/juridico/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from apps.notificacoes.models import Notificacoes
 from apps.juridico.models import Contrato
@@ -30,7 +30,52 @@ def novo_contrato(request):
         formulario.save()
         return HttpResponseRedirect("/juridico/")
     return render(request, 'juridico/contrato_form.html',
-                  {'form': form,
-                   'qtd_notificacao': qtd_notificacao,
-                   'notificacoes_menu': notificacoes_menu,
-                   })
+                  {
+                      'form': form,
+                      'qtd_notificacao': qtd_notificacao,
+                      'notificacoes_menu': notificacoes_menu,
+                  })
+
+
+def editar_contrato(request, id):
+    qtd_notificacao = Notificacoes.qtd_notificacoes(request.user.id)
+    notificacoes_menu = Notificacoes.listar_notificacoes_menu(request.user.id)
+    contrato = get_object_or_404(Contrato, pk=id)
+    form = ContratoForm(request.POST or None, instance=contrato)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/juridico/listar_contratos/")
+    return render(request, 'juridico/contrato_form.html',
+                  {
+                      'form': form,
+                      'qtd_notificacao': qtd_notificacao,
+                      'notificacoes_menu': notificacoes_menu,
+                  })
+
+
+def deletar_contrato(request, id):
+    qtd_notificacao = Notificacoes.qtd_notificacoes(request.user.id)
+    notificacoes_menu = Notificacoes.listar_notificacoes_menu(request.user.id)
+    contrato = get_object_or_404(Contrato, pk=id)
+    form = ContratoForm(request.POST or None, instance=contrato)
+    if request.method == 'POST':
+        contrato.delete()
+        return HttpResponseRedirect("/juridico/listar_contratos/")
+    return render(request, 'juridico/contrato_delete_confirm.html',
+                  {
+                      'contrato': contrato,
+                      'qtd_notificacao': qtd_notificacao,
+                      'notificacoes_menu': notificacoes_menu,
+                  })
+
+
+def listar_contratos(request):
+    qtd_notificacao = Notificacoes.qtd_notificacoes(request.user.id)
+    notificacoes_menu = Notificacoes.listar_notificacoes_menu(request.user.id)
+    listar_contratos = Contrato.listar_contratos()
+    return render(request, 'juridico/listar_contratos.html',
+                  {
+                      'listar_contratos': listar_contratos,
+                      'qtd_notificacao': qtd_notificacao,
+                      'notificacoes_menu': notificacoes_menu,
+                  })
